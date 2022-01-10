b9d20dfec06cbff6db1be19304c9055aaf943e78
Mark Darvell <markd@docsol.com.au>
2022-01-10 15:24:46 +1000

    Added More Notes for Context

--- Start-ManagedFolderAssistant.ps1
+++ Start-ManagedFolderAssistant.ps1
@@ -1,3 +1,9 @@
+# Note After doing a PST Import retention settings around the Mailbox wouldn't apply 
+# Found the issue is once a PST Import is done a Retention Hold is enabled on the Mailbox 
+# The Below Commands show to to idenify if a mailbox has the Retention Setting Enabled and how to switch off and start the 
+# Managed Folder Assistant, this kicks off the rentention settings faster.
+
+
 # See if Rentention is enables we're wanting this to be false
 get-mailbox "MailboxName" | Select RetentionHoldEnabled
 
